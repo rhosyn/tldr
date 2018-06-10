a38b42d9b67e24d4c289932419cb6574a26a6e51
Bridget Burnett <sscott2203@gmail.com>
2018-06-10 19:45:13 +0300

    updated smmry model for url

--- lib/services/smmry.rb
+++ lib/services/smmry.rb
@@ -3,10 +3,10 @@ require 'json'
 
 class Smmry
   def summarize
-    url = "http://api.smmry.com/&SM_API_KEY=#{ENV['Smmry_API_Key']}&SM_WITH_BREAK&SM_WITH_ENCODE&SM_QUOTE_AVOID&SM_QUESTION_AVOID&SM_EXCLAMATION_AVOID&SM_KEYWORD_COUNT=10&SM_LENGTH=10&SM_URL=#{news_url}"
     Article.all.each do |a|
       if a.smmry_content != nil?
         news_url = a.newsapi_articleurl
+        url = "http://api.smmry.com/&SM_API_KEY=#{ENV['Smmry_API_Key']}&SM_WITH_BREAK&SM_WITH_ENCODE&SM_QUOTE_AVOID&SM_QUESTION_AVOID&SM_EXCLAMATION_AVOID&SM_KEYWORD_COUNT=10&SM_LENGTH=10&SM_URL=#{news_url}"
         smmry = JSON.parse(open(url).read)
         a.smmry_keywords = smmry["sm_api_keyword_array"]
         a.smmry_content = smmry["sm_api_content"].strip.gsub(/\&#039;/, "'").split("[BREAK]").each {|s| s.strip } if smmry["sm_api_content"]
