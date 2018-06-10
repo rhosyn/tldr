26446de8a263c9f1c805371ac53d13643f483abf
Bridget Burnett <sscott2203@gmail.com>
2018-06-10 15:44:23 +0300

    basic data acq setup and news api gem

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,18 @@
+require 'news-api'
+
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
 
   def home
+    # @news = getnews
   end
+
+
+  def getnews
+    # newsapi = News.new("2e708082020e4249b6dd845def387af1")
+    # top_headlines = newsapi.get_top_headlines(category: 'business', language: 'en')
+  end
+
+
 end
+
