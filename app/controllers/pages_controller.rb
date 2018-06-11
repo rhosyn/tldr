dc162f38e0defa2e247ddcf378eed7b6778cd3de
Bridget Burnett <sscott2203@gmail.com>
2018-06-11 14:32:13 +0300

    carousel updated

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
 
   def home
-    @articles = Article.all
+    @article = Article.first
   end
 
 end
