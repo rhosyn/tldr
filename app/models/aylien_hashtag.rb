af53b445722092fec00da35455c379c621136c6d
Bridget Burnett <38064401+rhosyn@users.noreply.github.com>
2018-06-13 14:36:38 +0300

    Update aylien_hashtag.rb

--- app/models/aylien_hashtag.rb
+++ app/models/aylien_hashtag.rb
@@ -1,4 +1,4 @@
 class AylienHashtag < ApplicationRecord
-  has_many :aylien_article_hashtags, dependent: :destroy
-  has_many :aylien_articles, through: :aylien_article_hashtags
+  has_many :article_hashtags, dependent: :destroy
+  has_many :aylien_articles, through: :article_hashtags
 end
