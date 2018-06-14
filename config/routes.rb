95f442295256b71a038b4ddb361b5940552ab990
Bridget Burnett <sscott2203@gmail.com>
2018-06-14 11:56:49 +0300

    integrating aylien frontend

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+  get 'index', to: 'pages#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   require "sidekiq/web"
   authenticate :user, lambda { |u| u.admin } do
