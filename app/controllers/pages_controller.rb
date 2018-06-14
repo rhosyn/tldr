b4437d6c1204d8e25b1d7fc17038dbf1529dc4d0
Bridget Burnett <sscott2203@gmail.com>
2018-06-14 18:53:28 +0300

    carousel reload for categories built

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,6 +3,22 @@ class PagesController < ApplicationController
 
   def home
     @aylien_articles = AylienArticle.all.reject {|a| a.summary_sentences.nil? }
+    @frontend_articles = []
+    @aylien_articles.each do |article|
+      cats = ArticleCategory.where(aylien_article: article)
+      iab = cats.select{|c| c.aylien_category.iab.length <= 5 } if cats.select{|c| c.aylien_category.iab.length <= 5 } != []
+      if iab && article.summary_sentences != nil
+        carousel_item = {
+                        id: article.id,
+                        title: article.title,
+                        image_url: article.img_url,
+                        category: iab[0].aylien_category.category,
+                        sentiment: article.title_polarity_sentiment
+                        }
+        @frontend_articles << carousel_item
+      end
+    end
+
     @aylien_snippets = {}
     @aylien_articles.each do |aylien|
       ay_snippets = aylien.summary_sentences.gsub!("[","").gsub!("]","").split("\",").to_a
