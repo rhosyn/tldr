12c23236b276f0f68dd7bfe4eef77d04c0162b15
Jason Begleiter <jhbegs204@gmail.com>
2018-06-19 15:27:17 +0300

    Placeholder commit

--- app/assets/javascripts/history.js
+++ app/assets/javascripts/history.js
@@ -4,16 +4,18 @@ function history_listeners() {
     element.addEventListener("click", (event) => {
       console.log(event.currentTarget);
       event.currentTarget.classList.toggle('history_active');
-      // document.querySelector('.history_overlay').classList.toggle('active');
-      // document.querySelector('.history_expanded_container').classList.toggle('active');
       document.querySelector('.history_container').classList.toggle('smaller');
+      // $('.overlay').toggleClass('active');
+      document.querySelector('.history_overlay').classList.toggle('active');
+      // document.querySelector('.history_expanded_container').classList.toggle('active');
       generate_history_expanded(event.currentTarget);
     })
   })
 }
 
 function history_overlay_listeners() {
-  document.querySelector('.history_overlay').addEventListener("click", (event) => {
+  console.log("hi");
+  document.querySelector('.overlaybox').addEventListener("click", (event) => {
     document.querySelector('.history_overlay').classList.toggle('active');
     document.querySelector('.history_container').classList.toggle('smaller');
     document.querySelector('.history_expanded_container').remove();
@@ -27,11 +29,11 @@ function history_overlay_listeners() {
 function generate_history_expanded (element) {
   // console.log(snippets);
   let container = document.createElement("div");
-  container.className = "history_expanded_container active";
+  container.className = "history_expanded_container";
 
   let img = document.createElement("div");
   img.className = "history_expanded_img";
-  img.setAttribute("style", `background-image: url:${element.dataset.img_url}`)
+  img.setAttribute("style", `background-image: url(${element.dataset.img_url});`)
   let title = document.createElement("p");
   title.innerHTML = element.dataset.title;
 
@@ -42,14 +44,15 @@ function generate_history_expanded (element) {
   snippet_container.classList = "history_expanded_snippet_container";
   const snippets = element.dataset.sentences.split('", "');
   snippets.forEach((s) => {
-
+    let snippet = document.createElement("div");
+    snippet.classList = "history_expanded_snippet";
+    snippet.innerHTML = s.replace('["','').replace(']"','');
+    snippet_container.appendChild(snippet);
   })
+
+  container.appendChild(snippet_container);
+  $( ".history_overlaycontainer" ).append(container);
 }
-
-
-
-
-
 
 $('document').ready(history_listeners())
 
